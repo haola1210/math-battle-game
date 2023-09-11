ed245a4d926b5a3dcfbc7c125a2bbf12ae9a671e
Vo Van Huy <huy.vo@mobifone.vn>
2023-09-11 10:19:38 +0700

    done join room socket

--- src/pages/Lobby/hooks/useLobbyLogic.tsx
+++ src/pages/Lobby/hooks/useLobbyLogic.tsx
@@ -40,8 +40,6 @@ export const useLobbyLogic = () => {
     socket?.current?.on(
       USER_EVENT.CREATE_ROOM_FEEDBACK,
       async ({ user, room }: { user: IUser; room: IRoom }) => {
-        console.log('here CREATE_ROOM_FEEDBACK ');
-
         if (user.username === auth.user?.username) {
           navigate(`${LINK.WAITING_ROOM}/${room._id}`);
           toast(`Create ${room.room_name} success !`);
@@ -71,10 +69,9 @@ export const useLobbyLogic = () => {
     socket?.current?.on(
       USER_EVENT.JOIN_ROOM_FEEDBACK_LOBBY,
       async ({ room, user_joined }: { room: IRoom; user_joined: IUser }) => {
-        console.log('here JOIN_ROOM_FEEDBACK_LOBBY ');
-
         if (user_joined.username === auth.user?.username) {
           navigate(`${LINK.WAITING_ROOM}/${room._id}`);
+          toast('Join room success!');
         } else {
           const tmpList = roomList.map((item) => (item._id === room._id ? room : item));
           setRoomList(tmpList);
@@ -90,7 +87,6 @@ export const useLobbyLogic = () => {
   useEffect(() => {
     iIFE(async () => {
       const roomList = await getRoomList();
-
       setRoomList(roomList);
     });
   }, []);
@@ -105,8 +101,6 @@ export const useLobbyLogic = () => {
   };
 
   const handleJoinRoom = (room_id: string) => {
-    console.log('here');
-
     const payload = {
       user_id: auth.user?._id,
       room_id,
--- src/pages/WaitingRoom/hooks/useWaitingRoomLogic.tsx
+++ src/pages/WaitingRoom/hooks/useWaitingRoomLogic.tsx
@@ -1,19 +1,51 @@
-import { type IUser } from '@interfaces/user.interface';
+import { LINK } from '@constants/link';
+import { USER_EVENT } from '@constants/room-event';
+import { useSocket } from '@contexts/SocketContext';
+import { IRoom } from '@interfaces/room.interfaces';
+import { IUser } from '@interfaces/user.interface';
 import { getRoomInfo } from '@services/room.service';
 import { iIFE } from '@utils/IIFE';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const useWaitingRoomLogic = () => {
   const [roommate, setRoommate] = useState<IUser[]>([]);
   const { room_id } = useParams();
+  const socket = useSocket();
+  const navigate = useNavigate();
 
   useEffect(() => {
     iIFE(async () => {
       const data = await getRoomInfo(room_id ?? ``);
-      setRoommate(data.users);
+      if (data) {
+        setRoommate(data?.users);
+      } else {
+        toast('This room is not exist!');
+        navigate(LINK.LOBBY);
+      }
     });
   }, []);
+
+  // join room feedback room
+  useEffect(() => {
+    socket?.current?.on(
+      USER_EVENT.JOIN_ROOM_FEEDBACK_ROOM,
+      async ({ user_joined }: { user_joined: IUser }) => {
+        try {
+          const tempRoommate = [...roommate, user_joined];
+          setRoommate(tempRoommate);
+        } catch (error) {
+          toast('Something went wrong!');
+        }
+      },
+    );
+
+    return () => {
+      socket?.current?.off();
+    };
+  }, []);
+
   return { roommate };
 };
 
